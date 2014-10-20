aeb2a5b2c33de231b21bc20311486f2c2165360c
Haylin Ellenes <haylin-chama@hotmail.es>
2014-10-20 10:53:50 -0300

    Create main.cpp
    Menu

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "Par.h"
+#include#include <iostream>
+#include "20141020cab.h"
+
+using namespace std; <iostream>
+#include "20141020cab.h"
+
+using namespace std;
+using namespace std;
+
+int main()
+{
+    cout << "Clase PAR" << endl;
+    Par P1;
+    P1.ver();
+    P1.SetPar(2,3);
+    P1.ver();
+
+    Par R;
+    R.SetPar(5,6);
+    R.ver();
+
+    cout << "Par x : " << P1.GetParX() << endl;
+    cout << "Par y : " << P1.GetParY() << endl;
+
+    return 0;
+}
